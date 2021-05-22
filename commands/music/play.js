676c073b444aaa71ac289493148f4efa0d8d46f5
Glitch (well-childlike-auroraceratops) <none>
2021-05-22 12:43:23 +0000

    🦁👶 Checkpoint ./commands/music/play.js:23487586/20 ./commands/music/search.js:23487586/126 ./commands/music/end.js:23487586/18

--- commands/music/play.js
+++ commands/music/play.js
@@ -67,7 +67,7 @@ module.exports = {
       req: message.author
     };
 
-    if (serverQueue) {
+    if (serverQueue&&serverQueue!==[]) {
       message.channel.stopTyping();
       serverQueue.songs.push(song);
       let thing = new MessageEmbed()
--- commands/music/search.js
+++ commands/music/search.js
@@ -168,7 +168,7 @@ module.exports = {
       req: message.author
     };
 
-    if (serverQueue) {
+    if (serverQueue&&serverQueue!==[]) {
       message.channel.stopTyping();
       serverQueue.songs.push(song);
       let thing = new MessageEmbed()
