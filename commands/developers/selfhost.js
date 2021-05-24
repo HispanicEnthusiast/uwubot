78cd177fe4e0554ef1d28587ab527a4465ed8bf2
Glitch (tairitsu-bot-discord) <none>
2021-05-24 17:22:49 +0000

    🐏👰 Checkpoint ./commands/developers/selfhost.js:23487586/234

--- commands/developers/selfhost.js
+++ commands/developers/selfhost.js
@@ -1,7 +1,7 @@
 exports.run = (bot, message, args) => {
    const { MessageEmbed } = require('discord.js');
   let a = new MessageEmbed()
-    .setDescription('You want to host me on your own? Then let\'s go!!!')
+    .setDescription('You want to host me on your own? Then let\'s go!!! <:hikaristaire:801419553783414815>\nHere\'s the [link](https://github.com/t404owo/t404owo-bot-discord/blob/main/SELF-HOSTING.md)')
     message.mentionReply(a)
   };
   
@@ -9,7 +9,7 @@ exports.run = (bot, message, args) => {
       name: 'selfhost',
   aliases:['selfhosting'],
   usage: "",
-    description: "get the uptime of the bot",
+    description: "if u want to selfhost the bot <:lilith:801419553896661022>",
   };
 exports.conf={
   cooldown: 0,
