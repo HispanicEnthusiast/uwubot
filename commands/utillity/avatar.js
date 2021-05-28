b2e1aaba293816f95603f0bcdf2276f7b6470bb1
Glitch (tairitsu-bot-discord) <none>
2021-05-28 21:13:13 +0000

    🌕🎈 Checkpoint ./commands/utillity/avatar.js:23487586/1044

--- commands/utillity/avatar.js
+++ commands/utillity/avatar.js
@@ -31,34 +31,41 @@ exports.run = async (bot, message, args) => {
   
 }
 exports.interaction = async (bot, message, arg) => {
-let args
+let args=[arg.find(arg => arg.name.toLowerCase() == "user").value]
   let use;
 
     if (!args[0]) {
-      use = message.member;
+      use = interaction.member_id;
     } else {
       if(!message.guild) return;
-      use = await bot.guilds.cache.get(message.guild_id).members.fetch().catch(err => { return message.mentionReply("<:tairitsuno:801419553933492245> | Please Mention a correct user or give a correct id of the user!") })
+      use = await bot.guilds.cache.get(message.guild_id).members.fetch(args[0].replace("<@!","").replace("<@", "").replace(">","")).catch(err => { return message.mentionReply("<:tairitsuno:801419553933492245> | Please Mention a correct user or give a correct id of the user!") })
     
     
     }
 
     if (!use) {
-      return message.mentionReply("<:tairitsuno:801419553933492245> | Unable to find this person!")
+      return bot.api.interactions(message.id, message.token).callback.post({
+                data: {
+                    type: 4,
+                    data: {
+                        content: "<:tairitsuno:801419553933492245> | Unable to find this person!"
+                    }
+                }
+            });
     }
-  if(message.author.id === "770304260919001159"){
-   
-              
-  
-  }
   // user.avatarURL({dynamic: true, size: 1024});
-  console.log(use.user.avatarURL({dynamic: true, size: 1024}))
+ // console.log(use.user.avatarURL({dynamic: true, size: 1024}))
   let embed = new discord.MessageEmbed()
   .setTitle(`${use.user.tag}`)
   .setDescription(`[Avatar link]`+`(${use.user.avatarURL({dynamic: true, size: 1024})})`)
   .setImage(`${use.user.avatarURL({dynamic: true, size: 1024})}`)
   .setColor(use.displayHexColor === "#000000" ? "#ffffff" : use.displayHexColor)
-  message.noMentionReply(embed)
+  bot.api.interactions(message.id, message.token).callback.post({
+                data: {
+                    type: 4,
+                    data: await bot.createAPIMessage(message, embed)
+                }
+            });
   
 }
 exports.options
