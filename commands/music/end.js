d78bff726e51d2cb178948b26bccd67f993a22cd
Glitch (tairitsu-bot-discord) <none>
2021-05-29 19:50:05 +0000

    📇🌎 Checkpoint ./commands/music/end.js:23487586/1156

--- commands/music/end.js
+++ commands/music/end.js
@@ -1,6 +1,4 @@
 const { MessageEmbed } = require("discord.js");
-const sendError = require("../../util/error");
-const sendSuccess = require("../../util/success");
 
 module.exports = {
   conf:{
@@ -15,6 +13,9 @@ module.exports = {
   },
 //checked
   run: async function (client, message, args) {
+    const sendError = require("../../util/error");
+const sendSuccess = require("../../util/success");
+
     const channel = message.member.voice.channel
     if (!channel)return sendError('<:tairitsuno:801419553933492245> | You need to join a voice channel to use this command!', message);
     if (message.guild.me.voice.channel !== channel)return sendError('<:tairitsuno:801419553933492245> | You need to join voice channel where the bot is to use this command!', message);
@@ -27,6 +28,17 @@ module.exports = {
   },
   options:[],
   interaction: async function (client, message, args){//message=interaction
-   client.guilds.cache.get(message.guild_id).members.cache.get(message.member.user.id).voice.channel
+   let sendError=require('../../util/slash/error.js')
+    let sendSuccess=require('../../util/slash/success.js')
+   let channel=client.guilds.cache.get(message.guild_id).members.cache.get(message.member.user.id).voice.channel
+    if (!channel)return sendError('<:tairitsuno:801419553933492245> | You need to join a voice channel to use this command!', message, client);
+    
+    if (message.guild.me.voice.channel !== channel)return sendError('<:tairitsuno:801419553933492245> | You need to join voice channel where the bot is to use this command!', message, client);
+    await channel.leave();
+    message.react("801419553841741904")
+    sendSuccess("<:hikariok:801419553841741904> | Disconnected Successfully!", message, client);
+    const serverQueue = client.guilds.cache.get(message.guild_id).client.queue.get(message.guild_id);
+
+    if(serverQueue){client.guilds.cache.get(message.guild_id).client.queue.delete(message.guild_id);console.log("disconnected")}
   }
 };
