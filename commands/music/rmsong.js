96481a82795306a7141b0f88643114e3125ea295
Glitch (tairitsu-bot-discord) <none>
2021-05-30 08:36:27 +0000

    👡👙 Checkpoint ./commands/music/rmsong.js:23487586/1558

--- commands/music/rmsong.js
+++ commands/music/rmsong.js
@@ -1,6 +1,5 @@
 const { MessageEmbed } = require("discord.js");
-const sendSuccess = require("../../util/success")
-const sendError =require("../../util/error")
+
 module.exports = {
   conf:{
     cooldown: 0,
@@ -25,8 +24,32 @@ module.exports = {
             "qurm"]
   }
   ,
-  run: (bot, message, args) => {
+  interaction: (bot, message, arg) => {
+    const sendSuccess = require("../../util/slash/success")
+const sendError =require("../../util/slash/error")
+    let args=[]
+if(arg)args=[arg.find(arg => arg.name.toLowerCase() == "song").value]  
+    const channel = message.member.voice.channel
+    if (!channel)return sendError('<:tairitsuno:801419553933492245> | You need to join a voice channel to use this command!', message);
+    if (message.guild.me.voice.channel !== channel)return sendError('<:tairitsuno:801419553933492245> | You need to join voice channel where the bot is to use this command!', message);
+
+    const serverQueue = message.client.queue.get(message.guild.id);
+
+    if (!serverQueue)return sendError("<:tairitsuno:801419553933492245> | There is nothing playing in this server.", message);
+     if(isNaN(args[0]))return sendError("<:tairitsuno:801419553933492245> | Please use Numerical Values only", message)
+    if(args[0]<2)return sendError("<:tairitsuno:801419553933492245> | Please give a number that is higher than 1", message)
+   
+    if(args[0] > serverQueue.songs.length) {
+      return sendError("<:tairitsuno:801419553933492245> | Unable to find this song", message)
+    }
     
+    
+    serverQueue.songs.splice(args[0] - 1, 1)
+    sendSuccess("<:hikariok:801419553841741904> | Song is removed sucessfully!", message)
+  },
+  run: (bot, message, args) => {
+    const sendSuccess = require("../../util/success")
+const sendError =require("../../util/error")
     const channel = message.member.voice.channel
     if (!channel)return sendError('<:tairitsuno:801419553933492245> | You need to join a voice channel to use this command!', message);
     if (message.guild.me.voice.channel !== channel)return sendError('<:tairitsuno:801419553933492245> | You need to join voice channel where the bot is to use this command!', message);
