36704a20f4da1fdb9ee0e0614b9d1239a7e84627
Glitch (tairitsu-bot-discord) <none>
2021-05-30 06:57:22 +0000

    💇🐯 Checkpoint ./commands/utillity/addslash.js:23487586/1466

---
+++ commands/utillity/addslash.js
@@ -0,0 +1,45 @@
+const { MessageEmbed } = require("discord.js");
+
+const sendError =require("../../util/success"), fs=require('fs')
+exports.run = (bot, message, args) => {
+  
+      if (message.member.hasPermission("MANAGE_GUILD")||bot.config.owners.includes(message.author.id)||message.member.hasPermission("MANAGE_CHANNELS")||message.member.hasPermission("ADMINISTRATOR")||message.guild){
+
+      
+    fs.readdir("./commands/", (err, categories) => {
+	if (err) console.log(err);
+  categories.forEach(category => {
+    let moduleConf = require(`../${category}/module.json`);
+    moduleConf.path = `../${category}`;
+    moduleConf.cmds = [];
+    if (!moduleConf) return;
+    bot.helps.set(category, moduleConf);
+
+    fs.readdir(`./commands/${category}`, (err, files) => {
+      if (err) console.log(err);
+
+      files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        let prop = require(`../${category}/${file}`);
+        let cmdName = file.split(".")[0];
+        if(!prop.options||!prop.interaction)return
+        
+      
+
+        bot.helps.get(category).cmds.push(prop.info.name);
+
+bot.api.applications(bot.user.id).guilds(message.guild.id.toString()).commands.post({
+        data: {
+            name: prop.info.name,
+            description: prop.info.description,
+	     options:prop.options
+        }
+    });//command for slash
+
+ })
+})
+})
+})
+   return message.noMentionReply(
+        `<:hikariok:801419553841741904> | Slash command succesfully exported!`
+      );
