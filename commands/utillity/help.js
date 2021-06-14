9c8c7efd836224e9afaf9b8098ab221b1661b509
Glitch (solid-night-fennel) <none>
2021-06-14 21:29:27 +0000

    👋🛥 Checkpoint ./commands/utillity/help.js:4486980/4085

--- commands/utillity/help.js
+++ commands/utillity/help.js
@@ -1,23 +1,26 @@
 const discord = require('discord.js')
 exports.run = (bot, message, args) => {
-const version = process.env.VERSION;
+  const version = process.env.VERSION;
 
-client.on('message', message => {
-if (message.content === '?help') {
-    
-const MessageEmbed = new Discord.MessageEmbed()
-      .setColor('#78d7fa')
-      .setThumbnail(bot.user.avatarURL({dynamic:true, size: 1024}))
-      .setTitle('Haunt Bot Commands - Help Hub')
-      .setDescription(`Utility: ?invite | ?support | ?prefix | ?avatar | ?info | ?whois\nQuery: ?calculate | ?anime\nModeration: ?kick | ?ban | ?mute | ?warn | ?purge | ?warnings\nEconomy: ?bal | ?daily | ?roll | ?bet | ?give\nMusic: ?play | ?np | ?lyrics | ?pause | ?search | ?queue | ?skip | ?vol\nFun: ?kiss | ?hug | ?pat | ?slap | ?wink | ?cuddle\nNSFW: ?neko | ?foxgirl`)
-      .setFooter('Thank you so much for choosing Haunt, it means the world to me! <3');
-message.channel.send(MessageEmbed);
+let embed = new discord.MessageEmbed()
   
+    .setTitle("Haunt Bot Commands")
+  
+    .setDescription('Utility: ?invite | ?support | ?prefix | ?avatar | ?info | ?whois', 'Query: ?calculate | ?anime', 'Moderation: ?kick | ?ban | ?mute | ?warn | ?purge | ?warnings', 'Economy: ?bal | ?daily | ?roll | ?bet | ?give', 'Music: ?play | ?np | ?lyrics | ?pause | ?search | ?queue | ?skip | ?vol', 'Fun: ?kiss | ?hug | ?pat | ?slap | ?wink | ?cuddle', 'NSFW: ?neko | ?foxgirl')
+  
+    .setThumbnail(bot.user.avatarURL({dynamic:true, size: 1024}))
+    .setFooter("Thank you so much for choosing Haunt, it means the world to me! <3")
+    .setColor("#78d7fa")
+
+}
 exports.info = {
   name: 'help',
   aliases: ['h'],
   usage: "",
-  description: "show the commands"
+  description: "shows the info of the bot"
 }
-}
+
+exports.conf={
+  cooldown: 0,
+  dm: "yes"
 }
