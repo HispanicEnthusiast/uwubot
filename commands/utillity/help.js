04a2a96a25a832c996020407005801cd3ab29390
Glitch (solid-night-fennel) <none>
2021-06-14 21:53:26 +0000

    🛰🔋 Checkpoint ./commands/utillity/help.js:4486980/5296

--- commands/utillity/help.js
+++ commands/utillity/help.js
@@ -1,10 +1,16 @@
 const discord = require('discord.js')
 exports.run = (bot, message, args) => {
-  const version = process.env.VERSION;
+const version = process.env.VERSION;
 
 let embed = new discord.MessageEmbed()
     .setTitle("Haunt Bot Commands")
-    .setDescription('Utility: ?invite | ?support | ?prefix | ?avatar | ?info | ?whois', 'Query: ?calculate | ?anime', 'Moderation: ?kick | ?ban | ?mute | ?warn | ?purge | ?warnings', 'Economy: ?bal | ?daily | ?roll | ?bet | ?give', 'Music: ?play | ?np | ?lyrics | ?pause | ?search | ?queue | ?skip | ?vol', 'Fun: ?kiss | ?hug | ?pat | ?slap | ?wink | ?cuddle', 'NSFW: ?neko | ?foxgirl')
+    .setDescription('Utility: ?invite | ?support | ?prefix | ?avatar | ?info | ?whois')
+    .setDescription('Query: ?calculate | ?anime')
+    .setDescription('Moderation: ?kick | ?ban | ?mute | ?warn | ?purge | ?warnings')
+    .setDescription('Economy: ?bal | ?daily | ?roll | ?bet | ?give')
+    .setDescription('Music: ?play | ?np | ?lyrics | ?pause | ?search | ?queue | ?skip | ?vol')
+    .setDescription('Fun: ?kiss | ?hug | ?pat | ?slap | ?wink | ?cuddle')
+    .setDescription('NSFW: ?neko | ?foxgirl')
     .setThumbnail(bot.user.avatarURL({dynamic:true, size: 1024}))
     .setFooter("Thank you so much for choosing Haunt, it means the world to me! <3")
     .setColor("#78d7fa")
