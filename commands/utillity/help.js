4c75ea69ac4609ebeaef51c3e1d9d55b2b60f5d1
Glitch (solid-night-fennel) <none>
2021-06-14 20:54:28 +0000

    🌼🏕 Checkpoint ./commands/utillity/help.js:4486980/3513

--- commands/utillity/help.js
+++ commands/utillity/help.js
@@ -1,27 +1,10 @@
-let discord = require("discord.js");
-
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-exports.run = async (client, message, args) => {
-    let user;
-    if (message.mentions.users.first()) {
-      if(!message.guild) return;
-        user = message.mentions.users.first();
-    } else if (args[0]) {
-      if(!message.guild) return;
-        user = message.guild.members.cache.get(args[0]).user;
-    } else {
-        user = message.author;
-    }
-
-    const embed = new Discord.MessageEmbed()
-    .setColor(0x7289DA)
-    .setTitle(`Haunt's Bot Commands`)
+const discord = require('discord.js')
+exports.run = (bot, message, args) => {
+  const version = process.env.VERSION;
+  let embed = new discord.MessageEmbed()
+  
+    .setTitle("Haunt Bot Commands")
+  
     .addField("Utility: ?invite | ?support | ?prefix | ?avatar | ?info | ?whois")
     .addField("Query: ?calculate | ?anime")
     .addField("Moderation: ?kick | ?ban | ?mute | ?warn | ?purge | ?warnings")
@@ -29,11 +12,21 @@ exports.run = async (client, message, args) => {
     .addField("Music: ?play | ?join | ?np | ?lyrics | ?pause/continue | ?search | ?queue | ?skip/skipto | ?volume | ?stop | ?disconnect")
     .addField("Fun: ?kiss | ?hug | ?pat | ?slap | ?wink | ?cuddle")
     .addField("NSFW: ?neko | ?foxgirl")
-    .setThumbnail(user.displayAvatarURL({size: 4096, dynamic: true}))
-    .setTimestamp(new Date) 
+  
+    .setThumbnail(bot.user.avatarURL({dynamic:true, size: 1024}))
+    .addField("Bot Developer:", process.env.DISCORD_BOT_OWNER, true)
+    .setFooter("Thank you so much for choosing Haunt, it means the world to me! <3")
+    .setColor("#78d7fa")
+    
     return message.noMentionReply(embed);
-}
 
+}
+exports.info = {
+  name: 'help',
+  aliases: ['h'],
+  usage: "",
+  description: "shows the info of the bot"
+}
 exports.conf = {
   cooldown: 5,
   dm: "yes"
