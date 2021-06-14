dd96050333ed833305be7a0eb85cdcc5fda27806
Glitch (solid-night-fennel) <none>
2021-06-14 21:41:27 +0000

    🐣⛺️ Checkpoint ./commands/utillity/help.js:4486980/87

--- commands/utillity/help.js
+++ commands/utillity/help.js
@@ -3,16 +3,13 @@ exports.run = (bot, message, args) => {
   const version = process.env.VERSION;
 
 let embed = new discord.MessageEmbed()
-  
     .setTitle("Haunt Bot Commands")
-  
     .setDescription('Utility: ?invite | ?support | ?prefix | ?avatar | ?info | ?whois', 'Query: ?calculate | ?anime', 'Moderation: ?kick | ?ban | ?mute | ?warn | ?purge | ?warnings', 'Economy: ?bal | ?daily | ?roll | ?bet | ?give', 'Music: ?play | ?np | ?lyrics | ?pause | ?search | ?queue | ?skip | ?vol', 'Fun: ?kiss | ?hug | ?pat | ?slap | ?wink | ?cuddle', 'NSFW: ?neko | ?foxgirl')
-  
     .setThumbnail(bot.user.avatarURL({dynamic:true, size: 1024}))
     .setFooter("Thank you so much for choosing Haunt, it means the world to me! <3")
     .setColor("#78d7fa")
-
 }
+
 exports.info = {
   name: 'help',
   aliases: ['h'],
