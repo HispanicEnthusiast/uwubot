ec562ed395c4a988c7c1e05ab49f98a4561dda5a
Glitch (uwuthebot) <none>
2021-06-26 02:35:05 +0000

    ⏰💋 Checkpoint ./main.js:4486980/1638

--- main.js
+++ main.js
@@ -532,15 +532,19 @@ const promises = [
   )
 ];
 
-const activities_list = [ 
-    "Playing", 
-    "Watching"
-    ]; // creates an arraylist containing phrases you want your bot to switch through.
+ const activities_list = [ 
+    " ", 
+    "Hell",
+    "Discord Users",
+    "The 'uwu help' Command",
+    "Time Pass",
+    "Life & Loving My Family"
+    ]; 
 
 bot.on('ready', () => {
     setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 6); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        bot.user.setActivity(activities_list[index]({type: "WATCHING"}));
     }, 10000); // Runs this every 10 seconds.
 });
 
